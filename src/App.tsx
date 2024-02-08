3c5759cb66dfc50e899de7f3f9b0d4ebb8233428
Seth Thompson <thompsonrussell22@gmail.com>
2024-02-08 17:23:01 -0500

    complete HTML and CSS Module without red boxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import memeimage from "./assets/meme-image.png";
 
 function App(): JSX.Element {
     return (
@@ -11,6 +13,28 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World
             </p>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <img
+                            src={memeimage}
+                            alt="Meme that reads the html does not need to load css & js if the html and css are in js"
+                        />
+                    </Col>
+                    <Col>
+                        <h1 className="header">Project Technologies:</h1>
+                        <ul>
+                            <li>Typescript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                        </ul>
+                        <Button onClick={() => console.log("Hello World!")}>
+                            Log Hello World
+                        </Button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
