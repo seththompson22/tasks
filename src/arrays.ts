f5ea5555c2bb324c566f181be51b6ee73074a756
Seth Thompson <thompsonrussell22@gmail.com>
2024-02-15 17:50:46 -0500

    completed tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -13,7 +13,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    return numbers.map((numbers: number) => numbers * 3);
 }
 
 /**
