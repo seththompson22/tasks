4f753ed1f26a18f1aad9edc2865c2a226a3119df
Seth Thompson <thompsonrussell22@gmail.com>
2024-02-23 19:26:04 -0500

    completed toMarkdown

--- src/objects.ts
+++ src/objects.ts
@@ -93,7 +93,13 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let ret = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        question.options.map(
+            (value: string): string => (ret += "\n" + "- " + value)
+        );
+    }
+    return ret;
 }
 
 /**
